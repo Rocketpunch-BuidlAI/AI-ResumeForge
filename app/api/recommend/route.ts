87ee828f600689f574668443525c23cf5d9c24b7
dongjun <dongjun.lee@rocketpunch.com>
2025-04-11 21:20:08 +0900

    fix: error

--- app/api/recommend/route.ts
+++ app/api/recommend/route.ts
@@ -1,6 +1,8 @@
+import { NextResponse } from "next/server";
+
 // TODO: 상호님이 구현해주신 API 호출
 export async function POST(request: Request) {
   const { searchParams } = new URL(request.url);
-
+  const user = searchParams.get('user');
   return NextResponse.json(user);
 }
