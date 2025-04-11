6e82e6d34d89e5ebf0521c1b011515fa6f4cbbb6
dongjun <dongjun.lee@rocketpunch.com>
2025-04-11 21:38:01 +0900

    fix: update next.config.ts for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+import { Configuration } from 'webpack';
+
 const nextConfig = {
   output: 'standalone',
   experimental: {
     serverActions: true,
+  },
+  webpack: (config: Configuration) => {
+    if (config.resolve) {
+      config.resolve.fallback = { fs: false, path: false };
+    }
+    return config;
   },
 };
 
